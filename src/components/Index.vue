e57317b165be0671ccf5aba64deacb0d5f778753
Tatsuro <contacts@tatsuro.dev>
2019-05-31 17:25:18 +0900

    deleteメソッド作成して、アタッチ(41)

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -33,6 +34,29 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter(smoothie => {
+        return smoothie.id !== id;
+      });
+      /* 配列操作系のメソッドまとめ
+      forEach((val, index, array) => {
+        // 破壊的(元の配列が変わる)
+      });
+      map((val, index, array) => {
+        // ただのコピー
+      });
+      filter((val, index, array) => {
+        return 条件;
+        // ただのコピー、trueになった要素だけを返す
+      });
+      slice(index, endIndex);
+      // コピー、endIndexは未満の終了位置
+      splice(index, removedNum);
+      // 破壊的、removedNumは元の配列から削除する要素の個数
+      */
+    }
   }
 };
 </script>
@@ -55,5 +79,13 @@ export default {
 }
 .index .ingredients li {
   display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
 }
 </style>
