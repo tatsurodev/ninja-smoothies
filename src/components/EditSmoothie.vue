5b53db0a670667fe139ff2e49f5d314ee93c8434
Tatsuro <contacts@tatsuro.dev>
2019-05-31 23:50:53 +0900

    EditSmoothieコンポーネントのルート、リンク作成(52)

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="edit-smoothie container">
+    <!-- このページのルートパラメータは、this.$route.params.param_nameで所得できる -->
+    <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "EditSmoothie",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
